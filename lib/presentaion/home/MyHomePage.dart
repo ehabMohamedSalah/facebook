11efdd5aef691af8222547b70439d0f7017131c7
ehab salah <ehabsalah52t@gmail.com>
2024-05-06 15:45:46 +0300

    separate postItem from homepage

--- lib/presentaion/home/MyHomePage.dart
+++ lib/presentaion/home/MyHomePage.dart
@@ -4,28 +4,25 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:task1_work/core/utils/Strings_manager.dart';
 import 'package:task1_work/core/utils/assets_manager.dart';
-
-import '../../core/resuableComponent/states2_item.dart';
+import 'package:task1_work/presentaion/home/widget/Posts.dart';
 
 class MyHomePage extends StatelessWidget {
-List<String> lists=["Reels","You","Chat","Guide","frind","Reels","You","Chat","Guide","frind"];
-List<String> feeling=["Anonymous post","feeling","location","Guide","frind","Reels","You","Chat","Guide","frind"];
+  List<String> lists=["Reels","You","Chat","Guide","frind","Reels","You","Chat","Guide","frind"];
+  List<String> feeling=["Anonymous post","feeling","location","Guide","frind","Reels","You","Chat","Guide","frind"];
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.transparent,
       child: Scaffold(
-
         appBar: AppBar(
         //  backgroundColor: Colors.lightBlue,
           leading: Icon(Icons.arrow_back,color: Colors.white,),
           actions: [
-
             IconButton(onPressed:  (){}, icon:  Icon(Icons.search,color: Colors.white,),),
             IconButton(onPressed:  (){}, icon:  Icon(Icons.mobile_screen_share_outlined,color: Colors.white,),),
             IconButton(onPressed:  (){}, icon:   Icon(Icons.list,color: Colors.white,), )
-
           ],
         ),
         body: SingleChildScrollView(
@@ -99,14 +96,12 @@ List<String> feeling=["Anonymous post","feeling","location","Guide","frind","Ree
               ),
               SizedBox(height: 20.h,),
               Container(
-          
                 height: 50.h,
                 child: ListView.separated(
                   separatorBuilder: (context, index) => SizedBox(width: 10.w,),
                   scrollDirection: Axis.horizontal,
                   itemCount: lists.length,
                   itemBuilder: (context, index) =>
-
                    Container(
                      width: 70.w,
                        height: 100.h,
@@ -114,109 +109,10 @@ List<String> feeling=["Anonymous post","feeling","location","Guide","frind","Ree
                         child: Center(child: Text(lists[index],style: Theme.of(context).textTheme.labelMedium?.copyWith(fontWeight: FontWeight.bold),))),),
                 ),
               SizedBox(height: 20.h,),
-          
-          
-              Container(color: Colors.black12,height: 5.h,),
-              SizedBox(height: 15.h,),
-          
-              Padding(
-                padding:   REdgeInsets.all(8.0),
-                child: Row(children: [
-                  Text("Featured",style: Theme.of(context).textTheme.headlineMedium,),
-                  Icon(Icons.info,color: Colors.black,),
-                  Spacer(),
-                  Icon(Icons.arrow_drop_down_outlined),
-                ],),
-              ),
-              SizedBox(height: 15.h,),
-              Container(color: Colors.black12,height: 5.h,),
-          
-              Padding(
-                padding:   REdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    CircleAvatar(
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(40),
-                          child: Image.asset("assets/images/pic.png",height: 40.sp,)),
-                    ),
-                    Container(
-                      width: 300.w,
-                      height: 50.h,
-                      child: TextFormField(
-          
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(40.sp)
-                          ),
-                          hintText: 'Write something...', // Hint text
-          
-                        ),),
-                    ),
-                    Icon(Icons.image,color: Colors.greenAccent,size: 35.sp,)
-                  ],
-                ),
-              ),
-              Container(color: Colors.black12,width: double.infinity,height: 1,),
+              PostsWidget(),
 
-          Container(
-            height: 40.h,
-            child: ListView.separated(
-              separatorBuilder: (context, index) => SizedBox(width: 10.w,),
-              scrollDirection: Axis.horizontal,
-              itemCount: lists.length,
-              itemBuilder: (context, index) =>SecondStates(feeling[index]),
 
-            ),
-          ),
-              SizedBox(height: 15.h,),
-              Container(color: Colors.black12,width: double.infinity,height: 1,),
-              Padding(
-                padding:   REdgeInsets.only(right: 30,left: 30,top: 20,bottom: 20),
-                
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                  Text("Most Relevant",style: Theme.of(context).textTheme.headlineMedium,),
-                  Icon(Icons.format_list_bulleted_outlined),
-                ],),
-              ),
-              Container(color: Colors.black12,width: double.infinity,height: 1,),
-              Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(color: Colors.orange,borderRadius: BorderRadius.circular(40.sp)),
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(40.sp),
-                        child: Icon(Icons.person,color: Colors.white,size: 40.sp,)),
-                  ),
-                  SizedBox(width: 10.w,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                    Container(
-                      width: 300.w,
-                      child: Row(
-                        children: [
-                          Text("Anonymous participant",style: Theme.of(context).textTheme.headlineMedium),
-                          Spacer(),
-                          Icon(Icons.linear_scale_outlined)
-                        ],
-                      ),
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                      Text("9h"),
-                      SizedBox(width: 10.w,),
-                      Icon(Icons.public)
-                    ],)
-                  ],)
-                ],
-              )
-          
-          
+
           
             ],
           ),
